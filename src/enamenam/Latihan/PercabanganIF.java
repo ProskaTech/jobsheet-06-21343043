c82dbd091d7b0a410407654b7cd5b54a15c5055b
21343043 <113020517+21343043@users.noreply.github.com>
2022-10-06 19:42:23 +0700

    Add files via upload

---
+++ src/enamenam/Latihan/PercabanganIF.java
@@ -0,0 +1,20 @@
+package enamenam.Latihan;
+
+import java.util.Scanner;
+
+// Created by 21343043_Fadhil Nugraha Wikarya
+
+public class PercabanganIF {
+    public static void main(String[] args) {
+        double total_pembelian, diskon = 0;
+
+        Scanner dataMasuk = new Scanner(System.in);
+        System.out.print("Masukan Total Pembelian: Rp ");
+        total_pembelian = dataMasuk.nextDouble();
+
+        if (total_pembelian >= 50000)
+            diskon = 0.1 * total_pembelian;
+
+        System.out.println("Besarnya diskon : Rp " + diskon);    
+    }
+}
